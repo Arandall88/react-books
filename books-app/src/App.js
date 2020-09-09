d1f93833c9988c990b29f23ab08d2efa73939f3e
Angel Randall <drivenbysuccess21@gmail.com>
2020-09-08 20:35:24 -0500

    added table and table data added edit and delete buttons

--- books-app/src/App.js
+++ books-app/src/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { Table, Button } from 'reactstrap';
+
 
 function App() {
   return (
-    <div className="App">
-     
+
+    <div className="App container">
+     <Table>
+       <thead>
+         <tr>
+           
+             <th>#</th>
+             <th>Title</th>
+             <th>Rating</th>
+             <th>Actions</th>
+         </tr>
+       </thead>
+       <tbody>
+         <tr>
+           <td>1</td>
+           <td>Book title</td>
+           <td>2.5</td>
+           <td>
+              <Button color="success" size="sm">Edit</Button>
+              <Button color="danger" size="sm">Delete</Button>
+           </td>
+         </tr>
+       </tbody>
+
+     </Table>
     </div>
   );
 }
